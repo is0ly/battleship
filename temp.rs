e8d4b112743f4ab0b5ba1b41ada2d63f071cc418
Ilya Osipov <iljaosipov@gmail.com>
2025-04-16 11:33:03 +0300

    Create temp.rs

---
+++ temp.rs
@@ -0,0 +1,84 @@
+use std::collections::HashSet;
+use std::io;
+
+// battleship - 1
+// cruiser - 2
+//  destroyer - 3
+// motortorpedoboot - 4
+
+struct NavalFleet {
+    
+}
+
+
+struct Ship {
+    name: String,
+    decks_amount: u8,
+    occupied: HashSet<(u8, u8)>,
+    is_live: bool,
+}
+
+fn main() {
+    let mut battleship = create_ship("Battleship", 4);
+    
+    println!("{}", battleship.name);
+    println!("{}", battleship.decks_amount);
+    println!("{}", battleship.is_live);
+    
+    battleship.is_live = false;
+    
+    print_field(battleship);
+
+}
+
+fn print_field(ship: Ship) {
+    for n in 0..10 {
+        for o in 0..10 {
+            let coords = (n, o);
+            if ship.occupied.contains(&coords) {
+                print!("{} ", 'x');
+                continue;
+            }
+            if o == 9 {
+                println!("");
+                continue;
+            }
+            print!("{} ", 'o');
+        }
+    }
+}
+
+fn get_user_coordinates() -> (u8, u8) {
+    let mut point = String::new();
+
+    println!("Enter coordinates-> ");
+
+    io::stdin()
+        .read_line(&mut point)
+        .expect("Failed to read line");
+
+    let digits: Vec<u8> = point
+        .trim()
+        .chars()
+        .map(|c| c.to_digit(10).unwrap() as u8)
+        .collect();
+
+    let coordinates = (digits[0], digits[1]);
+
+    coordinates
+}
+
+fn create_ship(name: &str, decks_amount: u8) -> Ship {
+    let mut battleship_occupied: HashSet<(u8, u8)> = HashSet::new();
+
+    for _n in 0..decks_amount {
+        battleship_occupied.insert(get_user_coordinates());
+    }
+
+    Ship {
+        name: name.to_string(),
+        decks_amount: 4,
+        occupied: battleship_occupied,
+        is_live: true,
+    }
+}
