4f9abdc3a0bc7299169e1c61339ec0da1b65f85f
Ilya Osipov <iljaosipov@gmail.com>
2025-04-16 13:51:39 +0300

    Update temp.rs

--- temp.rs
+++ temp.rs
@@ -31,6 +31,7 @@ impl ShipType  {
 
 struct Ship {
     name: String,
+    ship_type: ShipType,
     decks_amount: u8,
     occupied: HashSet<(u8, u8)>,
     is_live: bool,
@@ -42,34 +43,42 @@ struct NavalFleet {
 
 fn main() {
     // let mut battleship = create_ship("Battleship", 4);
+    
+    let battleship = Ship {
+        name: String::from("Battleship"),
+        ship_type: ShipType::Battleship,
+        decks_amount: ShipType::Battleship.decks(),
+        occupied: HashSet::from([(1,1), (1, 2), (1, 3), (1, 4)]),
+        is_live: true,
+    };
 
-    let mut player_naval_fleet = create_naval_fleet();
+    // let mut player_naval_fleet = create_naval_fleet();
 
-    println!("{}", player_naval_fleet.battleship[0].name);
-    println!("{}", player_naval_fleet.battleship[0].decks_amount);
-    println!("{}", player_naval_fleet.battleship[0].is_live);
+    // println!("{}", player_naval_fleet.battleship[0].name);
+    // println!("{}", player_naval_fleet.battleship[0].decks_amount);
+    // println!("{}", player_naval_fleet.battleship[0].is_live);
 
-    player_naval_fleet.battleship[0].is_live = false;
+    // player_naval_fleet.battleship[0].is_live = false;
 
-    print_field(player_naval_fleet);
+    // print_field(player_naval_fleet);
 }
 
-fn print_field(user_naval_fleet: NavalFleet) {
-    for n in (1..11).rev() {
-        for o in 1..11 {
-            let coords = (n, o);
-            if user_naval_fleet.battleship[0].occupied.contains(&coords) {
-                print!("{} ", 'x');
-                continue;
-            }
-            if o == 10 {
-                println!("");
-                continue;
-            }
-            print!("{} ", 'o');
-        }
-    }
-}
+// fn print_field(user_naval_fleet: NavalFleet) {
+//     for n in (1..11).rev() {
+//         for o in 1..11 {
+//             let coords = (n, o);
+//             if user_naval_fleet.battleship[0].occupied.contains(&coords) {
+//                 print!("{} ", 'x');
+//                 continue;
+//             }
+//             if o == 10 {
+//                 println!("");
+//                 continue;
+//             }
+//             print!("{} ", 'o');
+//         }
+//     }
+// }
 
 fn get_user_coordinates() -> (u8, u8) {
     let mut point = String::new();
@@ -91,56 +100,56 @@ fn get_user_coordinates() -> (u8, u8) {
     coordinates
 }
 
-fn create_ship(name: &str, decks_amount: u8) -> Ship {
-    let mut battleship_occupied: HashSet<(u8, u8)> = HashSet::new();
+// fn create_ship(name: &str, decks_amount: u8) -> Ship {
+//     let mut battleship_occupied: HashSet<(u8, u8)> = HashSet::new();
 
-    for _n in 0..decks_amount {
-        battleship_occupied.insert(get_user_coordinates());
-    }
+//     for _n in 0..decks_amount {
+//         battleship_occupied.insert(get_user_coordinates());
+//     }
 
-    Ship {
-        name: name.to_string(),
-        decks_amount: 4,
-        occupied: battleship_occupied,
-        is_live: true,
-    }
-}
+//     Ship {
+//         name: name.to_string(),
+//         decks_amount: 4,
+//         occupied: battleship_occupied,
+//         is_live: true,
+//     }
+// }
 
-fn create_naval_fleet() -> NavalFleet {
-    let battleship_count: u8 = 1;
-    // let cruiser_count: u8 = 2;
-    // let destroyer_count: u8 = 3;
-    // let motortorpedoboot_count: u8 = 4;
+// fn create_naval_fleet() -> NavalFleet {
+//     let battleship_count: u8 = 1;
+//     // let cruiser_count: u8 = 2;
+//     // let destroyer_count: u8 = 3;
+//     // let motortorpedoboot_count: u8 = 4;
 
-    let mut battleships = vec![];
-    // let mut cruisers = vec![];
-    // let mut destroyers = vec![];
-    // let mut motortorpedoboots = vec![];
+//     let mut battleships = vec![];
+//     // let mut cruisers = vec![];
+//     // let mut destroyers = vec![];
+//     // let mut motortorpedoboots = vec![];
 
-    for _n in 0..battleship_count {
-        println!("Create battleship - 1");
-        battleships.push(create_ship("Battleship", 4));
-    }
+//     for _n in 0..battleship_count {
+//         println!("Create battleship - 1");
+//         battleships.push(create_ship("Battleship", 4));
+//     }
 
-    // for n in 0..cruiser_count {
-    //     println!("Create cruisers - 2");
-    //     battleships.push(create_ship("Cruiser", 3));
-    // }
+//     // for n in 0..cruiser_count {
+//     //     println!("Create cruisers - 2");
+//     //     battleships.push(create_ship("Cruiser", 3));
+//     // }
 
-    // for n in 0..destroyer_count {
-    //     println!("Create destroyers - 3");
-    //     battleships.push(create_ship("Destroyer", 2));
-    // }
+//     // for n in 0..destroyer_count {
+//     //     println!("Create destroyers - 3");
+//     //     battleships.push(create_ship("Destroyer", 2));
+//     // }
 
-    // for n in 0.. motortorpedoboot_count {
-    //     println!("Create motortorpedoboots - 4");
-    //     battleships.push(create_ship("Motortorpedoboot", 1));
-    // }
+//     // for n in 0.. motortorpedoboot_count {
+//     //     println!("Create motortorpedoboots - 4");
+//     //     battleships.push(create_ship("Motortorpedoboot", 1));
+//     // }
 
-    NavalFleet {
-        battleship: battleships,
-        // cruiser: cruisers,
-        // destroyer: destroyers,
-        // motortorpedoboot: motortorpedoboots,
-    }
-}
+//     NavalFleet {
+//         battleship: battleships,
+//         // cruiser: cruisers,
+//         // destroyer: destroyers,
+//         // motortorpedoboot: motortorpedoboots,
+//     }
+// }
