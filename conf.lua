67590e8011783ece44882a617d3436f839d727c3
Ilya Osipov <iljaosipov@gmail.com>
2025-04-27 14:27:31 +0300

    Update conf.lua

--- conf.lua
+++ conf.lua
@@ -9,3 +9,20 @@ vim.api.nvim_create_autocmd("ColorScheme", {
     vim.api.nvim_set_hl(0, "VertSplit", { bg = "none" })
   end,
 })
+
+
+
+
+return {
+  {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000, -- чтобы тема загрузилась раньше всего
+    opts = {
+      transparent_mode = true,
+    },
+    config = function(_, opts)
+      require("gruvbox").setup(opts)
+      vim.cmd("colorscheme gruvbox")
+    end,
+  },
+}
