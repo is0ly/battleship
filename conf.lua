68d935c1e2f1352fc71b03c594c214d0220fbfa2
Ilya Osipov <iljaosipov@gmail.com>
2025-04-27 14:25:24 +0300

    Update conf.lua

--- conf.lua
+++ conf.lua
@@ -1,1 +1,11 @@
 
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "*",
+  callback = function()
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+    vim.api.nvim_set_hl(0, "VertSplit", { bg = "none" })
+  end,
+})
